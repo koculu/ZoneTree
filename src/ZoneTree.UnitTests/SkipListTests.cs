35b2619b5864a1cbd042ec854aeba7b1532a9072
Ahmed Yasin Koculu <koculu@gmail.com>
2022-07-16 20:10:30 +0200

    Execute tests in parallel.

--- src/ZoneTree.UnitTests/SkipListTests.cs
+++ src/ZoneTree.UnitTests/SkipListTests.cs
@@ -81,28 +81,31 @@ public class SkipListTests
         var skipList = new SkipList<int, int>(new IntegerComparerAscending(), (int)Math.Log2(n) + 1);
         for (var i = 1; i < n; i += 2)
             skipList.Insert(i, i);
-        // 1 3 5 7 9
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(4).Key, Is.EqualTo(3));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(3).Key, Is.EqualTo(3));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(-1), Is.Null);
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(10).Key, Is.EqualTo(9));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(9).Key, Is.EqualTo(9));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(1).Key, Is.EqualTo(1));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(5).Key, Is.EqualTo(5));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(7).Key, Is.EqualTo(7));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(8).Key, Is.EqualTo(7));
-        Assert.That(skipList.GetLastNodeSmallerOrEqual(0), Is.Null);
+        Assert.Multiple(() =>
+        {
+            // 1 3 5 7 9
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(4).Key, Is.EqualTo(3));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(3).Key, Is.EqualTo(3));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(-1), Is.Null);
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(10).Key, Is.EqualTo(9));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(9).Key, Is.EqualTo(9));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(1).Key, Is.EqualTo(1));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(5).Key, Is.EqualTo(5));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(7).Key, Is.EqualTo(7));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(8).Key, Is.EqualTo(7));
+            Assert.That(skipList.GetLastNodeSmallerOrEqual(0), Is.Null);
 
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(-1).Key, Is.EqualTo(1));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(1).Key, Is.EqualTo(1));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(2).Key, Is.EqualTo(3));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(3).Key, Is.EqualTo(3));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(4).Key, Is.EqualTo(5));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(5).Key, Is.EqualTo(5));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(6).Key, Is.EqualTo(7));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(7).Key, Is.EqualTo(7));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(8).Key, Is.EqualTo(9));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(9).Key, Is.EqualTo(9));
-        Assert.That(skipList.GetFirstNodeGreaterOrEqual(10), Is.Null);
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(-1).Key, Is.EqualTo(1));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(1).Key, Is.EqualTo(1));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(2).Key, Is.EqualTo(3));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(3).Key, Is.EqualTo(3));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(4).Key, Is.EqualTo(5));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(5).Key, Is.EqualTo(5));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(6).Key, Is.EqualTo(7));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(7).Key, Is.EqualTo(7));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(8).Key, Is.EqualTo(9));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(9).Key, Is.EqualTo(9));
+            Assert.That(skipList.GetFirstNodeGreaterOrEqual(10), Is.Null);
+        });
     }
 }
