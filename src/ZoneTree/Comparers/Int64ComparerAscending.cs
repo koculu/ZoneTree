4f9561053ad14775293f966e5ae3bfb6f79e7b58
Ahmed Yasin Koculu <koculu@gmail.com>
2022-07-17 18:01:10 +0200

    Adds Int64 comparer.

---
+++ src/ZoneTree/Comparers/Int64ComparerAscending.cs
@@ -0,0 +1,14 @@
+﻿using Tenray.Collections;
+
+namespace Tenray;
+
+public class Int64ComparerAscending : IRefComparer<long>
+{
+    public int Compare(in long x, in long y)
+    {
+        var r = x - y;
+        if (r == 0)
+            return 0;
+        return r < 0 ? -1 : 1;
+    }
+}
