1d5554604c16d138289b58044e07651521972492
Ahmed Yasin Koculu <koculu@gmail.com>
2022-08-10 00:16:13 +0200

    Move benchmark.

--- src/Playground/Benchmark/ZoneTreeBenchmarks.cs
+++ src/Playground/Benchmark/ZoneTreeBenchmarks.cs
@@ -1,11 +1,10 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Engines;
-using Playground.Benchmark;
 using Tenray.ZoneTree.Core;
 using Tenray.ZoneTree.WAL;
 
-namespace Playground;
+namespace Playground.Benchmark;
 
 [HtmlExporter]
 [SimpleJob(RunStrategy.ColdStart, targetCount: 1)]
@@ -30,23 +29,23 @@ public class ZoneTreeBenchmarks
         TestConfig.MinimumSparseArrayLength = 0;
         TestConfig.DiskSegmentMode = DiskSegmentMode.MultipleDiskSegments;
     }
-    
+
     [Benchmark]
     public void Insert_1M_Lazy() => BenchmarkGroups.Insert1(1_000_000, WriteAheadLogMode.Lazy);
-    
+
     [Benchmark]
     public void Insert_3M1_Lazy() => BenchmarkGroups.Insert1(3_000_000, WriteAheadLogMode.Lazy);
-    
+
     [Benchmark]
     public void Insert_1M_Immediate() => BenchmarkGroups.Insert1(1_000_000, WriteAheadLogMode.Immediate);
-    
+
     [Benchmark]
     public void Insert_3M_Immediate() => BenchmarkGroups.Insert1(3_000_000, WriteAheadLogMode.Immediate);
-    
+
     [Benchmark]
     public void Insert_1M_CompressedImmediate() => BenchmarkGroups.Insert1(1_000_000, WriteAheadLogMode.CompressedImmediate);
-    
+
     [Benchmark]
     public void Insert_3M_CompressedImmediate() => BenchmarkGroups.Insert1(3_000_000, WriteAheadLogMode.CompressedImmediate);
-    
+
 }
