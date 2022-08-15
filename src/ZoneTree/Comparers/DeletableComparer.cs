fb355758e66a0be756fe97df74d1e27218d0c05a
Ahmed Yasin Koculu <koculu@gmail.com>
2022-08-15 17:44:28 +0200

    Adds deletable comparer

---
+++ src/ZoneTree/Comparers/DeletableComparer.cs
@@ -0,0 +1,27 @@
+﻿using Tenray.ZoneTree.Collections;
+using Tenray.ZoneTree.Core;
+
+namespace Tenray.ZoneTree.Comparers;
+
+public class DeletableComparer<TValue> : IRefComparer<Deletable<TValue>>
+{
+    public IRefComparer<TValue> Comparer { get; }
+
+    public DeletableComparer(IRefComparer<TValue> comparer)
+    {
+        Comparer = comparer;
+    }
+
+    public int Compare(in Deletable<TValue> x, in Deletable<TValue> y)
+    {
+        return Comparer.Compare(x.Value, y.Value);
+    }
+}
+
+public static class DeletableComparer
+{
+    public static IRefComparer<Deletable<TValue>> From<TValue>(IRefComparer<TValue> comparer)
+    {
+        return new DeletableComparer<TValue>(comparer);
+    }
+}
