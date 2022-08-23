72840b22cfc41590eb6b300711183f946c3c8ad5
Ahmed Yasin Koculu <koculu@gmail.com>
2022-08-23 23:14:09 +0200

    Update API docs.

--- src/ZoneTree/Core/DiskSegmentMode.cs
+++ src/ZoneTree/Core/DiskSegmentMode.cs
@@ -1,7 +1,19 @@
 ﻿namespace Tenray.ZoneTree.Core;
 
+/// <summary>
+/// Available Disk Segment Modes.
+/// </summary>
 public enum DiskSegmentMode
 {
+    /// <summary>
+    /// Disk segments are kept in single file.
+    /// Recommended for db size < sizeof(int) x 10M
+    /// </summary>
+    SingleDiskSegment,
+
+    /// <summary>
+    /// Disk Segments are partitioned into several files.
+    /// Recommended for db size > sizeof(int) x 10M
+    /// </summary>
     MultiPartDiskSegment,
-    SingleDiskSegment
 }
