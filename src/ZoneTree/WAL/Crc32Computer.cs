1ee8900f6493423e824a5c99b162e7639fe628db
Dmitriy Korolev <d.korolev@live.ru>
2022-11-14 14:53:32 +0700

    Arm support (#16)
    * Add ARM support
    
    * Add ARM disclamer + format

--- src/ZoneTree/WAL/Crc32Computer.cs
+++ src/ZoneTree/WAL/Crc32Computer.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Intrinsics.X86;
+﻿using X86 = System.Runtime.Intrinsics.X86;
+using Arm = System.Runtime.Intrinsics.Arm;
 
 namespace Tenray.ZoneTree.WAL;
 
@@ -6,17 +7,17 @@ public sealed class Crc32Computer
 {
     public static uint Compute(uint crc, ulong data)
     {
-        return (uint)Sse42.X64.Crc32(crc, data);
+        return ComputeCrc32(crc, data);
     }
 
     public static uint Compute(uint crc, uint data)
     {
-        return Sse42.Crc32(crc, data);
+        return ComputeCrc32(crc, data);
     }
 
     public static uint Compute(uint crc, int data)
     {
-        return Sse42.Crc32(crc, (uint)data);
+        return ComputeCrc32(crc, (uint)data);
     }
 
     public static uint Compute(uint crc, byte[] array)
@@ -25,17 +26,62 @@ public sealed class Crc32Computer
         var len = array.Length;
         while (len >= 8)
         {
-            crc = (uint)Sse42.X64.Crc32(crc, BitConverter.ToUInt64(array, off));
+            crc = ComputeCrc32(crc, BitConverter.ToUInt64(array, off));
             off += 8;
             len -= 8;
         }
 
         while (len > 0)
         {
-            crc = Sse42.Crc32(crc, array[off]);
+            crc = ComputeCrc32(crc, array[off]);
             off++;
             len--;
         }
         return crc;
+    }
+
+    private static uint ComputeCrc32(uint crc, ulong data)
+    {
+        if (X86.Sse42.X64.IsSupported)
+        {
+            return (uint)X86.Sse42.X64.Crc32(crc, data);
+        }
+        
+        if (Arm.Crc32.Arm64.IsSupported)
+        {
+            return Arm.Crc32.Arm64.ComputeCrc32C(crc, data);
+        }
+
+        throw new PlatformNotSupportedException();
+    }
+    
+    private static uint ComputeCrc32(uint crc, uint data)
+    {
+        if (X86.Sse42.IsSupported)
+        {
+            return X86.Sse42.Crc32(crc, data);
+        }
+        
+        if (Arm.Crc32.IsSupported)
+        {
+            return Arm.Crc32.ComputeCrc32C(crc, data);
+        }
+
+        throw new PlatformNotSupportedException();
+    }
+    
+    private static uint ComputeCrc32(uint crc, byte data)
+    {
+        if (X86.Sse42.IsSupported)
+        {
+            return X86.Sse42.Crc32(crc, data);
+        }
+        
+        if (Arm.Crc32.IsSupported)
+        {
+            return Arm.Crc32.ComputeCrc32C(crc, data);
+        }
+
+        throw new PlatformNotSupportedException();
     }
 }
