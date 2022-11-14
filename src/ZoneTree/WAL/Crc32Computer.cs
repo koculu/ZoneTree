379da693ad1b3da6290bb391c890f873e64f6930
Ahmed Yasin Koculu <koculu@gmail.com>
2022-11-14 21:27:33 +0100

    Added  Agressive inline compiler hint.

--- src/ZoneTree/WAL/Crc32Computer.cs
+++ src/ZoneTree/WAL/Crc32Computer.cs
@@ -1,25 +1,30 @@
 ﻿using X86 = System.Runtime.Intrinsics.X86;
 using Arm = System.Runtime.Intrinsics.Arm;
+using System.Runtime.CompilerServices;
 
 namespace Tenray.ZoneTree.WAL;
 
 public sealed class Crc32Computer
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static uint Compute(uint crc, ulong data)
     {
         return ComputeCrc32(crc, data);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static uint Compute(uint crc, uint data)
     {
         return ComputeCrc32(crc, data);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static uint Compute(uint crc, int data)
     {
         return ComputeCrc32(crc, (uint)data);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static uint Compute(uint crc, byte[] array)
     {
         var off = 0;
@@ -40,13 +45,14 @@ public sealed class Crc32Computer
         return crc;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static uint ComputeCrc32(uint crc, ulong data)
     {
         if (X86.Sse42.X64.IsSupported)
         {
             return (uint)X86.Sse42.X64.Crc32(crc, data);
         }
-        
+
         if (Arm.Crc32.Arm64.IsSupported)
         {
             return Arm.Crc32.Arm64.ComputeCrc32C(crc, data);
@@ -54,14 +60,15 @@ public sealed class Crc32Computer
 
         throw new PlatformNotSupportedException();
     }
-    
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static uint ComputeCrc32(uint crc, uint data)
     {
         if (X86.Sse42.IsSupported)
         {
             return X86.Sse42.Crc32(crc, data);
         }
-        
+
         if (Arm.Crc32.IsSupported)
         {
             return Arm.Crc32.ComputeCrc32C(crc, data);
@@ -69,14 +76,15 @@ public sealed class Crc32Computer
 
         throw new PlatformNotSupportedException();
     }
-    
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static uint ComputeCrc32(uint crc, byte data)
     {
         if (X86.Sse42.IsSupported)
         {
             return X86.Sse42.Crc32(crc, data);
         }
-        
+
         if (Arm.Crc32.IsSupported)
         {
             return Arm.Crc32.ComputeCrc32C(crc, data);
