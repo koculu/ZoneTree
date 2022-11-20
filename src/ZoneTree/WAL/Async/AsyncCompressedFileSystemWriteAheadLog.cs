c51f3177af58270ece2670205a6cb8d04990b2ac
Ahmed Yasin Koculu <koculu@gmail.com>
2022-11-20 21:36:50 +0100

    Reduce code duplication.

--- src/ZoneTree/WAL/Async/AsyncCompressedFileSystemWriteAheadLog.cs
+++ src/ZoneTree/WAL/Async/AsyncCompressedFileSystemWriteAheadLog.cs
@@ -174,46 +174,6 @@ public sealed class AsyncCompressedFileSystemWriteAheadLog<TKey, TValue> : IWrit
             FileStreamProvider.DeleteFile(tailPath);
     }
 
-    struct LogEntry
-    {
-        public long OpIndex;
-        public int KeyLength;
-        public int ValueLength;
-        public byte[] Key;
-        public byte[] Value;
-        public uint Checksum;
-
-        public uint CreateChecksum()
-        {
-            uint crc32 = 0;
-            if (Crc32Computer_SSE42_X64.IsSupported)
-            {
-                crc32 = Crc32Computer_SSE42_X64.Compute(crc32, (ulong)OpIndex);
-                crc32 = Crc32Computer_SSE42_X64.Compute(crc32, KeyLength);
-                crc32 = Crc32Computer_SSE42_X64.Compute(crc32, ValueLength);
-                crc32 = Crc32Computer_SSE42_X64.Compute(crc32, Key);
-                crc32 = Crc32Computer_SSE42_X64.Compute(crc32, Value);
-                return crc32;
-            }
-
-            if (Crc32Computer_ARM64.IsSupported)
-            {
-                crc32 = Crc32Computer_ARM64.Compute(crc32, (ulong)OpIndex);
-                crc32 = Crc32Computer_ARM64.Compute(crc32, KeyLength);
-                crc32 = Crc32Computer_ARM64.Compute(crc32, ValueLength);
-                crc32 = Crc32Computer_ARM64.Compute(crc32, Key);
-                crc32 = Crc32Computer_ARM64.Compute(crc32, Value);
-                return crc32;
-            }
-            throw new PlatformNotSupportedException();
-        }
-
-        public bool ValidateChecksum()
-        {
-            return CreateChecksum() == Checksum;
-        }
-    }
-
     void AppendLogEntry(byte[] keyBytes, byte[] valueBytes, long opIndex)
     {
         lock (AppendLock)
---
+++ src/ZoneTree/WAL/LogEntry.cs
@@ -0,0 +1,42 @@
+﻿namespace Tenray.ZoneTree.WAL;
+
+public struct LogEntry
+{
+    public long OpIndex;
+    public int KeyLength;
+    public int ValueLength;
+    public byte[] Key;
+    public byte[] Value;
+    public uint Checksum;
+
+    public uint CreateChecksum()
+    {
+        uint crc32 = 0;
+        if (Crc32Computer_SSE42_X64.IsSupported)
+        {
+            crc32 = Crc32Computer_SSE42_X64.Compute(crc32, (ulong)OpIndex);
+            crc32 = Crc32Computer_SSE42_X64.Compute(crc32, KeyLength);
+            crc32 = Crc32Computer_SSE42_X64.Compute(crc32, ValueLength);
+            crc32 = Crc32Computer_SSE42_X64.Compute(crc32, Key);
+            crc32 = Crc32Computer_SSE42_X64.Compute(crc32, Value);
+            return crc32;
+        }
+
+        if (Crc32Computer_ARM64.IsSupported)
+        {
+            crc32 = Crc32Computer_ARM64.Compute(crc32, (ulong)OpIndex);
+            crc32 = Crc32Computer_ARM64.Compute(crc32, KeyLength);
+            crc32 = Crc32Computer_ARM64.Compute(crc32, ValueLength);
+            crc32 = Crc32Computer_ARM64.Compute(crc32, Key);
+            crc32 = Crc32Computer_ARM64.Compute(crc32, Value);
+            return crc32;
+        }
+
+        throw new PlatformNotSupportedException();
+    }
+
+    public bool ValidateChecksum()
+    {
+        return CreateChecksum() == Checksum;
+    }
+}
