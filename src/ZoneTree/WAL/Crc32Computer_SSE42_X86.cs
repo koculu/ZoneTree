fbdea76913e7d44a94bc620e4ab8d42be53a4bff
Ahmed Yasin Koculu <koculu@gmail.com>
2023-09-13 20:31:49 +0200

    Add Crc32 x86 support.

---
+++ src/ZoneTree/WAL/Crc32Computer_SSE42_X86.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+
+namespace Tenray.ZoneTree.WAL;
+
+public sealed class Crc32Computer_SSE42_X86
+{
+    public static bool IsSupported => Sse42.IsSupported;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint Compute(uint crc, ulong data)
+    {
+        var result = (uint)Sse42.Crc32(crc, (uint)data);
+        return Sse42.Crc32(crc, (uint)(data >> 32));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint Compute(uint crc, uint data)
+    {
+        return Sse42.Crc32(crc, data);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint Compute(uint crc, int data)
+    {
+        return Sse42.Crc32(crc, (uint)data);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint Compute(uint crc, byte[] data)
+    {
+        var off = 0;
+        var len = data.Length;
+        while (len >= 4)
+        {
+            crc = (uint)Sse42.X64.Crc32(crc, BitConverter.ToUInt32(data, off));
+            off += 4;
+            len -= 4;
+        }
+
+        while (len > 0)
+        {
+            crc = Sse42.Crc32(crc, data[off]);
+            off++;
+            len--;
+        }
+        return crc;
+    }
+}
