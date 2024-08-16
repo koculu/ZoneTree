b63f18dd04cd91797d80fc30ed2e3be19292116b
Ahmed Yasin Koculu <koculu@gmail.com>
2024-08-17 00:49:37 +0200

    Add utility class and extension methods to simplify dealing with IRefComparer.

---
+++ src/ZoneTree/Comparers/ComparerFromRefComparer.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Tenray.ZoneTree.Comparers;
+
+public sealed class ComparerFromRefComparer<TKey> : IComparer<TKey>
+{
+    public readonly IRefComparer<TKey> RefComparer;
+
+    public ComparerFromRefComparer(IRefComparer<TKey> refComparer)
+    {
+        RefComparer = refComparer;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Compare(TKey x, TKey y)
+    {
+        return RefComparer.Compare(x, y);
+    }
+}
