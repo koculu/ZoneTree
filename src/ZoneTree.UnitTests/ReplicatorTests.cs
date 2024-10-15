7c97c3df21afd6d6b548463dffc58a1d2dccc1e8
Ahmed Yasin Koculu <koculu@gmail.com>
2024-10-15 08:13:34 +0200

    Stabilize replicaiton test case.

--- src/ZoneTree.UnitTests/ReplicatorTests.cs
+++ src/ZoneTree.UnitTests/ReplicatorTests.cs
@@ -5,6 +5,15 @@ namespace Tenray.ZoneTree.UnitTests;
 public sealed class ReplicatorTests
 {
     [Test]
+    public void TestReplicator2()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            TestReplicator();
+        }
+    }
+
+    [Test]
     public void TestReplicator()
     {
         var dataPath = "data/TestReplicator";
@@ -12,28 +21,25 @@ public sealed class ReplicatorTests
             Directory.Delete(dataPath, true);
         var recordCount = 50_000;
         var keyCount = 15_000;
-        var maxMemory = 10_000;
         void CreateData()
         {
             using var zoneTree = new ZoneTreeFactory<int, int>()
                 .SetDataDirectory(dataPath + "/source")
-                .SetMutableSegmentMaxItemCount(maxMemory)
                 .OpenOrCreate();
 
             using var replica = new ZoneTreeFactory<int, int>()
                 .SetDataDirectory(dataPath + "/replica")
-                .SetMutableSegmentMaxItemCount(maxMemory)
                 .OpenOrCreate();
 
             using var replicator = new Replicator<int, int>(replica, dataPath + "/replica-op-index");
             using var maintainer1 = zoneTree.CreateMaintainer();
             using var maintainer2 = replica.CreateMaintainer();
-            var random = new Random();
             int replicated = 0;
+            var k = 0;
             Parallel.For(0, recordCount, (i) =>
             {
                 var key = i % keyCount;
-                var value = random.Next();
+                var value = Interlocked.Increment(ref k);
                 var opIndex = zoneTree.Upsert(key, value);
                 Task.Run(() =>
                 {
@@ -60,6 +66,7 @@ public sealed class ReplicatorTests
 
             using var iterator1 = zoneTree.CreateIterator();
             using var iterator2 = replica.CreateIterator();
+            var i = 0;
             while (true)
             {
                 var n1 = iterator1.Next();
@@ -67,7 +74,9 @@ public sealed class ReplicatorTests
                 Assert.That(n2, Is.EqualTo(n1));
                 if (!n1) break;
                 Assert.That(iterator2.Current, Is.EqualTo(iterator1.Current));
+                ++i;
             }
+            Assert.That(i, Is.EqualTo(keyCount));
             zoneTree.Maintenance.Drop();
             replica.Maintenance.Drop();
         }
